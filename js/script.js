3cd0f5d7291de150da0f094ea0cab21f5ad07da7
Jhonatan <jhonatandavid.sanchezzea@riberadeltajo.es>
2026-03-02 12:04:41 +0100

    Mobile first design

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const menubtn = document.getElementById('menubtn');
+const mobilemenu = document.getElementById('mobilemenu');
+
+menubtn.addEventListener('click', () => {
+  mobilemenu.classList.toggle('mobile-menu--hidden');
+});
